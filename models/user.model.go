91f3f5a9d99209169ddea64067e40ca0bcd366dd
Toluwanimi <85632246+nimi-io@users.noreply.github.com>
2023-10-30 09:24:53 +0100

    Reset-Restart

--- models/user.model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type UserModel struct {
-	Id       string    `json:"id" bson:"_id" `
-	Username string `json:"username" bson:"username" binding:"required"`
-	Password string `json:"password" bson:"password" binding:"required"`
-}
-
